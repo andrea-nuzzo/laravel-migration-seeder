0271b34b18e021a341c2536d683210176fc3c158
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-02-07 16:19:11 +0100

    seeder with a loop

--- database/seeds/TravelPackagesSeeder.php
+++ database/seeds/TravelPackagesSeeder.php
@@ -14,14 +14,16 @@ class TravelPackagesSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $travel_package = new travel_package();
-        $travel_package->title_package = $faker->catchPhrase();
-        $travel_package->from = $faker->city();
-        $travel_package->to = $faker->city();
-        $travel_package->leaving_on = $faker->dateTimeThisYear();
-        $travel_package->returning_on = $faker->dateTimeThisYear('+7 days');
-        $travel_package->people = $faker->randomDigitNotNull();
-        $travel_package->all_inclusive= $faker->boolean();
-        $travel_package->save();
+        for($i = 0; $i < 10; $i++){
+            $travel_package = new travel_package();
+            $travel_package->title_package = $faker->catchPhrase();
+            $travel_package->from = $faker->city();
+            $travel_package->to = $faker->city();
+            $travel_package->leaving_on = $faker->dateTimeThisYear();
+            $travel_package->returning_on = $faker->dateTimeThisYear('+7 days');
+            $travel_package->people = $faker->randomDigitNotNull();
+            $travel_package->all_inclusive= $faker->boolean();
+            $travel_package->save();
+        }
     }
 }
