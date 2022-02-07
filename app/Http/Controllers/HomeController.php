311c966be8886841d23caf8ec7b1ce5e4ce5c602
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-02-07 15:13:05 +0100

    test created table with migrate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+//Collego il controller al modello
+use App\travel_package;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view ('welcome');
+        // Assegno ad una variabile tutta la tabella del DB travel_packages
+        $travel_package = travel_package::all();
+
+        return view ('home', compact("travel_package"));
     }
 }
